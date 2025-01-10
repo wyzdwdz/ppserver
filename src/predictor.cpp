a4d6355585457ece11568fa9e54821db49876d1a
wyzdwdz <wyzdwdz@gmail.com>
2025-01-10 17:10:43 +0100

    Update predictor.cpp

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -3,11 +3,11 @@
 void PpPredictor::InitializeCore(const std::string& engine)
 {
   pointpillar::lidar::VoxelizationParameter vp{};
-  vp.min_range = nvtype::Float3(-69.12, -39.68, -2.0);
-  vp.max_range = nvtype::Float3(69.12, 39.68, 16.0);
-  vp.voxel_size = nvtype::Float3(0.16, 0.16, 18.0);
+  vp.min_range = nvtype::Float3(-69.12f, -39.68f, -2.0f);
+  vp.max_range = nvtype::Float3(69.12f, 39.68f, 16.0f);
+  vp.voxel_size = nvtype::Float3(0.16f, 0.16f, 18.0f);
   vp.grid_size = vp.compute_grid_size(vp.max_range, vp.min_range, vp.voxel_size);
-  vp.max_voxels = 40000;
+  vp.max_voxels = 80000;
   vp.max_points_per_voxel = 32;
   vp.max_points = 300000;
   vp.num_feature = 4;
